d921bd8c12313486c952f5267f1c791bdee69d72
Marat <maratf76@gmail.com>
2025-11-21 11:17:07 +0500

    2025 11 21 1

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,31 +1,98 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace WebApplication1.Pages
 {
+
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
+        public IWebHostEnvironment WebHostEnvironment { get; set; }
+        public string webRootPath;
+        public string[] files;// = Directory.GetFiles(webRootPath);
+        public string[] directories;// = Directory.GetDirectories(webRootPath);
         public IndexModel(ILogger<IndexModel> logger)
         {
-            _logger = logger;
+            _logger = logger;            
+            WebHostEnvironment = GlobalData.Instance.environment;
         }
 
         public void OnGet()
         {
-
+            webRootPath = WebHostEnvironment.WebRootPath;
+            try
+            {
+                files = Directory.GetFiles(webRootPath);
+                directories = Directory.GetDirectories(webRootPath);
+            }
+            catch(Exception ex) 
+            {
+                files = new string[0];
+                directories = new string[0];
+                Debug.WriteLine(ex);
+            }
         }
         public async Task<IActionResult> OnPostTextClickAsync([FromBody] string text)
         {
-            if (string.IsNullOrEmpty(text))
-                return Content("Error: Text is empty");
-            // Обработка полученного текста
-            //await Task.Delay(100); // Имитация обработки
-            Debug.WriteLine(text);
-            // Возвращаем ответ
-            return Content($"You clicked on: {text}");
+            try
+            {
+                if (string.IsNullOrEmpty(text))
+                    return Content("Error: Text is empty");
+                // Обработка полученного текста
+                //await Task.Delay(100); // Имитация обработки
+                //Debug.WriteLine(text);
+                // Возвращаем ответ
+                string res = await ReadContent(text);// Task.Delay(1); // Имитация обработки
+                return Content(res);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return Content($"Error loading {text}");
+            }
+        }
+        public async Task<string> ReadContent(string content)
+        {
+            Debug.WriteLine(content);
+            string res = $"Ты нажал на: {content}";
+
+            string testContent = content.Trim();
+
+            try
+            {
+                bool t1 = Directory.Exists(testContent);
+                if (!t1)
+                {
+                    try
+                    {
+                        t1 = System.IO.File.Exists(testContent);
+                        if (t1)
+                        {
+                            System.IO.FileInfo fileInfo = new System.IO.FileInfo(testContent);
+                            string _ext = fileInfo.Extension;                            
+                            if (_ext.ToLower()==".txt")
+                            {
+                                StreamReader reader = fileInfo.OpenText();
+                                res = reader.ReadToEnd();
+                            }
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+
+            await Task.Delay(1);
+            return res;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -16,6 +16,9 @@ namespace WebApplication1
             {
                 app.UseExceptionHandler("/Error");
             }
+
+            GlobalData.Instance.environment = app.Environment;
+
             app.UseStaticFiles();
 
             app.UseRouting();
@@ -27,4 +30,79 @@ namespace WebApplication1
             app.Run();
         }
     }
+
+    public class GlobalData
+    {
+        #region Singleton
+        private static object syncRoot = new Object();
+        private static volatile GlobalData instance;
+        public static int programNum = 0;
+        public static string mutexString;
+        public static Mutex m = null;
+
+        public static GlobalData Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+
+                    bool doesNotExist = false;
+                    bool otherException = false;
+                    bool createdMutex = false;
+
+                    while (!createdMutex)
+                    {
+                        mutexString = "GeoLog" + programNum.ToString();
+                        try
+                        {
+                            // Open the mutex with (MutexRights.Synchronize |
+                            // MutexRights.Modify), to enter and release the
+                            // named mutex.
+                            //                                                       
+
+                            m = Mutex.OpenExisting(mutexString);
+
+
+                        }
+                        catch (WaitHandleCannotBeOpenedException)
+                        {
+                            Console.WriteLine("Mutex does not exist.");
+                            doesNotExist = true;
+                            m = new Mutex(false, mutexString, out createdMutex);
+                            continue;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Exception in mutex: {0}", ex.Message);
+                            otherException = true;
+                        }
+
+                        if ((m != null) && (!doesNotExist) && (!otherException))
+                        {
+                            // m.ReleaseMutex();
+                            m = null;
+                            programNum++;
+                        }
+
+                    }
+
+
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new GlobalData();
+                    }
+                }
+
+                return instance;
+            }
+        }
+        #endregion
+        
+        public IWebHostEnvironment environment;
+    
+    
+    }
+
 }
